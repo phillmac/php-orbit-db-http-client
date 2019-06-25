c3876cb7febea51e6a62668e9a3afee87be764b8
phillmac <phillmac@users.noreply.github.com>
2019-06-25 14:49:18 +1000

    Decode json to array

--- src/OrbitDBAPI.php
+++ src/OrbitDBAPI.php
@@ -58,7 +58,7 @@ class OrbitDBAPI
         } if ($response)  {
             $response_contents = $response->getBody();
             try {
-                $result = json_decode($response_contents);
+                $result = json_decode($response_contents, TRUE);
                 assert(! is_null($response), new Exception('Empty json response body'));
                 return $result;
             } catch (Exception $e){
