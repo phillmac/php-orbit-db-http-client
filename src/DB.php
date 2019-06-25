e24c54ecc3b09359134a431e88b6101387764e9f
phillmac <phillmac@users.noreply.github.com>
2019-06-25 18:09:56 +1000

    Debug

--- src/DB.php
+++ src/DB.php
@@ -88,6 +88,8 @@ class DB  {
 
     private function api_call (string $method, string $endpoint, array $json=[], array $options=[]) {
         $func_call = $this->func_call;
+        var_dump($func_call);
+        var_dump($method, $endpoint, $json, $options);
         return $func_call($method, $endpoint, $json, $options);
     }
 
